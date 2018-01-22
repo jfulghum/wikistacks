a3c6370654aa9e658bd3f776c3ebe72119a369f9
manja <anjiemerchant@gmail.com>
2018-01-22 10:48:39 -0500

    monday morning commit

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var morgan = require("morgan");
 var nunjucks = require("nunjucks");
 var router = require("./routes");
 var bodyParser = require("body-parser");
+const models = require("./models");
 
-app.engine("html", nunjucks.render); 
+app.engine("html", nunjucks.render);
 app.set('view engine', 'html');
 nunjucks.configure('views', {noCache: true});
 
@@ -14,9 +15,38 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 
-var server = app.listen(9999, function(){
-    console.log('listening on port 9999')
-});
+
+/* we could sync each model individually:
+// we need to sync our models before we starting listening on the port:
+models.User.sync({}) // a promise, when it resolves then then sync page.
+.then(function() {
+    return models.Page.sync({})
+    // we are returning to ensure that the syncing is complete before we move on.
+})
+.then(function() {
+    app.listen(9999, function() {
+        console.log('listening on port 9999')
+        // postgres is listening on 5432, so our app can't listen on 5432
+
+        // we want our app to initialize only after the db is set up
+    })
+})
+.catch(console.error);
+
+alternativel, we can sync the entire db at once
+*/
+
+// the force: true parameter will drop the table first and re-create it afterwards.  - if you force, existing tables will be overwritten
+
+
+models.db.sync({force: true})
+.then(function () {
+    app.listen(3000, function () {
+        console.log('Server is listening on port 3000!');
+    });
+})
+.catch(console.error);
+
 
 app.use(express.static("public"))
 app.use(router)
---
+++ models/index.js
@@ -0,0 +1,58 @@
+const Sequelize = require('sequelize');
+
+const db = new Sequelize('postgres://localhost:5432/wikistack', {
+  logging: false // by default sequelize will output the SQL command it makes to the database. here we're turning off the console logging.
+});
+// here we are connecting sequelize to the "currently running database process"
+
+const Page = db.define('page', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  urlTitle: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isURL: true
+    }
+  },
+  content: {
+    type: Sequelize.TEXT, // text here b/c "text" has an unlimited amount of characters whereas "string" has 255 char limit
+    allowNull: false
+  },
+  status: {
+    type: sequelize.ENUM('open', 'closed')// if the values are defined e.g. a picklist
+  },
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
+  }
+}, {
+  getterMethods: { // correct?
+    route: function() {
+      return '/wiki/' + this.urlTitle
+    }
+  }
+});
+
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+   }
+});
+
+
+module.exports = {
+  db: db,
+  Page: Page,
+  User: User
+}
